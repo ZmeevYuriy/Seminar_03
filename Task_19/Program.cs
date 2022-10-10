9d8a56bcaea42d0c247c28777fcb37b1d9e6a048
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-10 13:30:07 +0600

    add Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,32 @@
+﻿string GetText(string num)
+{
+    string result = string.Empty;
+    int length = num.Length;
+    int index = 0;
+    int z = length;
+    for (int i = 0; (num[i] == num[z - 1] && (i < z - 1)); i++)
+    {
+        z = z - 1;
+        index = i;
+    }
+    if (index == (length / 2) - 1) result = "да ";
+    else result = "нет";
+    return result;
+}
+int InputNumbers(string message)
+{
+    Console.WriteLine(message);
+    string number = Console.ReadLine();
+    int a = int.Parse(number);
+    return a;
+}
+
+int n = InputNumbers("Введите пятизначное число: ");
+    if (n > 10000 && n < 100000)
+    {
+        string stroka = Convert.ToString(n);
+        Console.Write(stroka + " -> ");
+        Console.WriteLine(GetText(stroka));
+        n++;
+    }
+    else Console.WriteLine("Введенное число не является пятизначным");
